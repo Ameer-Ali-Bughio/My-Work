7abcbf19675a9ab4d347e2dc33172284919e86fc
Ameer-Ali-Bughio <162275315+Ameer-Ali-Bughio@users.noreply.github.com>
2024-03-24 22:53:26 -0700

    first commit

---
+++ Numer_Guessing_Game/index.js
@@ -0,0 +1,14 @@
+import inquirer from "inquirer";
+const Random = Math.floor(Math.random() * 6 + 1);
+const answer = await inquirer.prompt([
+    { message: "Please Guess a Number 1-6",
+        type: "number",
+        name: "GuessedNumber" }
+]);
+if (answer.GuessedNumber == Random) {
+    console.log("Congratulation You Guessed Right Number");
+}
+else {
+    console.log("You Guessed Wrong Number");
+}
+;
