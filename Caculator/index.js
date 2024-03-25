030e7359b82d94243927aa7dc77fc1b21b4bf864
Ameer-Ali-Bughio <162275315+Ameer-Ali-Bughio@users.noreply.github.com>
2024-03-25 01:13:09 -0700

    first commit

---
+++ Caculator/index.js
@@ -0,0 +1,25 @@
+import inquirer from "inquirer";
+const answer = await inquirer.prompt([
+    { message: "Enter Value ", type: "number", name: "value1" },
+    { message: "Select Option to perform Action",
+        type: "list",
+        name: "Operators",
+        choices: ["+", "-", "*", "/",],
+    },
+    { message: "Enter Value ", type: "number", name: "value2" },
+]);
+if (answer.Operators === "+") {
+    console.log(answer.value1 + answer.value2);
+}
+else if (answer.Operators === "-") {
+    console.log(answer.value1 - answer.value2);
+}
+else if (answer.Operators === "*") {
+    console.log(answer.value1 * answer.value2);
+}
+else if (answer.Operators === "/") {
+    console.log(answer.value1 / answer.value2);
+}
+else {
+    console.log("Invalid Data");
+}
