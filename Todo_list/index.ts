cf0c8ec7e71ca0a5eb2db1ebd74d7a1ad5808398
Ameer-Ali-Bughio <162275315+Ameer-Ali-Bughio@users.noreply.github.com>
2024-03-30 06:03:45 -0700

    first commit

---
+++ Todo_list/index.ts
@@ -0,0 +1,28 @@
+#! /usr/bin/env node
+
+// make todo list
+import inquire from "inquirer";
+
+let todoList = [];
+
+let condition = true;
+while(condition){
+let answer = await inquire.prompt([
+    
+    {name: "todo", 
+    type: "input", 
+    message: "What do you Want to in your todoList"},
+
+    {name: "addmore",
+    type: "confirm",
+    message: "Do you want to add more?",
+    default: "false"},
+
+    
+]);
+
+
+todoList.push(answer.todo) 
+condition = answer.addmore;
+console.log(todoList)
+}
